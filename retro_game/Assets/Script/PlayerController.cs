ec3f159ef293ac1ffad0f39b2c8c2db856e3a1f8
KenzoLamn00000 <Lamn.mattias@maranathastudents.org>
2022-08-30 10:44:23 -0700

    15:58 part 5

--- retro_game/Assets/Script/PlayerController.cs
+++ retro_game/Assets/Script/PlayerController.cs
@@ -82,4 +82,10 @@ public class PlayerController : MonoBehaviour
 
         theAnimator.SetBool("Grounded", grounded); 
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Spike") {
+            Debug.Log("Ouch!");
+        }
+    }
 }
